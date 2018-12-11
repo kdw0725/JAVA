407dc824d97bce7931f213189793379a8bfc6af2
KDW <aron0725@naver.com>
2018-12-11 23:01:28 +0900

    12-11

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//System.out.println("Hello World!");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a+b);
+	
 	}
 
 }
