665e50cfa37ea4c396e8b77b0c99c9b988c3fb66
KDW <aron0725@naver.com>
2018-12-12 00:55:19 +0900

    12-12

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello World!");
+		//1¹ø
+		/*
+		System.out.println("Hello World!");
+		*/
 		
+		//2¹ø
+		/*
 		Scanner scanner = new Scanner(System.in);
-		
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
 		
 		System.out.println(a+b);
-	
+		*/
+		
+		//3¹ø
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a-b);
 	}
 
 }
